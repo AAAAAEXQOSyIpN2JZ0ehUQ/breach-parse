5b7fbd46a298ac87726cc845ed8e2b0095c628d1
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-10-10 03:55:59 -0300

    Actualizacion

--- README.md
+++ README.md
@@ -1,14 +1,103 @@
-# breach-parse
-A tool for parsing breached passwords
+![logo](https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/breach-parse/blob/main/Imagenes/breach-parse.jpg)
 
-### Installation
+# Findips :octocat:
 
-Install: `sudo ./install.sh`
+## :information_source: Descripción
+Breach-Parse v2: Herramienta de Parsing de Dominios Comprometidos
 
-Download breached password list from magnet located here: `magnet:?xt=urn:btih:7ffbcd8cee06aba2ce6561688cf68ce2addca0a3&dn=BreachCompilation&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fglotorrents.pw%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337`
+Este es un script de Bash diseñado para extraer credenciales de archivos filtrados en grandes colecciones de bases de datos de breaches. Automatiza el proceso de búsqueda de dominios específicos dentro de la Breach Compilation.
 
-If you don't store the password list (BreachCompilation) in `/opt/breach-parse`, specify the location like: 
+## :computer: Instalación
+```bash
+cd /opt
+sudo git clone https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/breach-parse
+cd breach-parse
+sudo chmod +x cd breach-parse/*
+cd breach-parse
+sudo ./install.sh
+ls -lthas
+```
+
+Descarga la lista de contraseñas violadas desde magnet, que se encuentra aquí:
+
+: `magnet:?xt=urn:btih:7ffbcd8cee06aba2ce6561688cf68ce2addca0a3&dn=BreachCompilation&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Fglotorrents.pw%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337`
+
+Si no almacena la lista de contraseñas (BreachCompilation) en `/opt/breach-parse`, especifique la ubicación de la siguiente manera:
 
 `breach-parse @gmail.com gmail.txt "~/Downloads/BreachCompilation/data"`
 
-Run `breach-parse` for instructions
+Ejecute `breach-parse` para obtener instrucciones
+## :rocket: Modo de Uso
+
+?? Ejemplos de Búsqueda para "hacker"
+
+?? Búsqueda en un dominio específico (por ejemplo, Gmail):
+```bash
+./breach-parse.sh "@gmail.com" gmail.txt; cat gmail-master.txt | grep -i "hacker"
+```
+
+?? Búsqueda en múltiples dominios:
+```bash
+./breach-parse.sh "@yahoo.com|@hotmail.com" multiple.txt; cat multiple-master.txt | grep -i "hacker"
+```
+
+?? Búsqueda en un dominio específico con archivo de salida personalizado:
+```bash
+./breach-parse.sh "@example.com" example.txt; cat example-master.txt | grep -i "hacker"
+```
+
+?? Búsqueda de un usuario específico (por ejemplo, admin) en un dominio:
+```bash
+./breach-parse.sh "@example.com" example.txt
+cat example-master.txt | grep -i "admin" | grep -i "hacker"
+```
+
+?? Búsqueda en un archivo existente:
+```bash
+cat existing_file.txt | grep -i "hacker"
+```
+
+??? Búsqueda en un dominio específico y luego en un archivo maestro:
+```bash
+./breach-parse.sh "@live.cn" live.txt; cat live-master.txt | grep -i "hacker"
+```
+
+?? Búsqueda de "hacker" en una lista de múltiples dominios:
+```bash
+./breach-parse.sh "@toyota.de|@live.cn" multiple.txt; cat multiple-master.txt | grep -i "hacker"
+```
+
+## :bookmark_tabs: Notas
+
+- El script busca archivos en la ubicación de la Breach Compilation y extrae coincidencias de correos electrónicos y contraseñas para el dominio proporcionado.
+- La salida incluye usuarios y contraseñas en archivos separados.
+- Para evitar problemas con espacios en las rutas, asegúrate de usar comillas dobles donde sea necesario.
+
+## :star2: Características 
+
+- Colores e Iconos: El script muestra el progreso y resultados utilizando colores y símbolos para una mejor visualización.
+- Barra de progreso: Durante el análisis, muestra el avance del procesamiento de los archivos.
+- Extracción automática: Se generan archivos separados para usuarios y contraseñas extraídos.
+- Prevención de duplicados: El script filtra automáticamente los duplicados para evitar resultados repetidos.
+
+## :hammer_and_wrench: Requisitos 
+
+- Sistema Operativo: Linux/Unix
+- Dependencias: Bash, grep, awk
+
+## :open_file_folder: Estructura del Repositorio
+
+| Icono            | Nombre              | Descripción                                      |
+|------------------|---------------------|--------------------------------------------------|
+| :file_folder:    | Imagenes            | Carpeta que contiene imágenes del script en ejecución |
+| :page_facing_up: | Findips_arp-scan    | Script principal que realiza un escaneo de red con arp-scan |
+| :page_facing_up: | Findips_nmap        | Script principal que realiza un escaneo de red con nmap |
+| :page_facing_up: | LICENSE             | Archivo de licencia MIT para el proyecto         |
+| :book:           | README.md           | Archivo README con la documentación del proyecto |
+| :package:        | install.sh          | Script de instalación automatizada               |
+
+## :email: Contacto 
+* :busts_in_silhouette: **Heath Adams**: [GitHub](https://github.com/hmaverickadams/breach-pars) - Desarrollador breach-pars 
+* :busts_in_silhouette: **Dzhoni**: [GitHub](https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/Findips) - Actualizacion Breach-Parse v2  
+
+? https://t.me/AAAAAEXQOSyIpN2JZ0ehUQ [  ???? ] ?
--- breach-parse.sh
+++ breach-parse.sh
@@ -1,59 +1,100 @@
 #!/usr/bin/env bash
+#====================================================
+#   SCRIPT:                   Breach Parse
+#   DESARROLLADO POR:         Heath Adams (hmaverickadams)
+#   ACTUALIZADO POR:          Jony Rivera (Dzhoni)
+#   FECHA DE ACTUALIZACI√ìN:   08-09-2024 
+#   CONTACTO POR TELEGRAMA:   https://t.me/Dzhoni_dev
+#   GITHUB OFICIAL:           https://github.com/AAAAAEXQOSyIpN2JZ0ehUQ/breach-parse
+#====================================================
+
+# Paleta de colores
+reset="\033[0m"       # Restablecer todos los estilos y colores
+bold="\033[1m"        # Texto en negrita
+italic="\033[3m"      # Texto en cursiva
+underline="\033[4m"   # Texto subrayado
+blink="\033[5m"       # Texto parpadeante
+reverse="\033[7m"     # Invertir colores de fondo y texto
+hidden="\033[8m"      # Texto oculto (generalmente invisible)
+
+# Colores de texto
+black="\033[0;30m"     # Negro
+red="\033[0;31m"       # Rojo
+green="\033[0;32m"     # Verde
+yellow="\033[0;33m"    # Amarillo
+blue="\033[0;34m"      # Azul
+magenta="\033[0;35m"   # Magenta
+cyan="\033[0;36m"      # Cian
+white="\033[0;37m"     # Blanco
+
+# Colores de fondo
+bg_black="\033[0;40m"     # Fondo Negro
+bg_red="\033[0;41m"       # Fondo Rojo
+bg_green="\033[0;42m"     # Fondo Verde
+bg_yellow="\033[0;43m"    # Fondo Amarillo
+bg_blue="\033[0;44m"      # Fondo Azul
+bg_magenta="\033[0;45m"   # Fondo Magenta
+bg_cyan="\033[0;46m"      # Fondo Cian
+bg_white="\033[0;47m"     # Fondo Blanco
+
+# Iconos v3
+checkmark="${white}[${green}+${white}]${green}"
+error="${white}[${red}-${white}]${red}"
+info="${white}[${yellow}*${white}]${yellow}"
+unknown="${white}[${blue}!${white}]${blue}"
+process="${white}[${magenta}>>${white}]${magenta}"
+indicator="${red}==>${cyan}"
+
+# Barra de separaci√≥n
+barra="${blue}|--------------------------------------------|${reset}"
+bar="${yellow}----------------------------------------------${reset}"
+
 
 if [ $# -lt 2 ]; then
-    echo "Breach-Parse v2: A Breached Domain Parsing Tool by Heath Adams"
-    echo " "
-    echo "Usage: ./breach-parse.sh <domain to search> <file to output> [breach data location]"
-    echo "Example: ./breach-parse.sh @gmail.com gmail.txt"
-    echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation/data"'
-    echo "You only need to specify [breach data location] if its not in the expected location (/opt/breach-parse/BreachCompilation/data)"
-    echo " "
-    echo 'For multiple domains: ./breach-parse.sh "<domain to search>|<domain to search>" <file to output>'
-    echo 'Example: ./breach-parse.sh "@gmail.com|@yahoo.com" multiple.txt'
+    echo -e "${error} Breach-Parse v2: A Breached Domain Parsing Tool by Heath Adams"
+    echo -e "${info} Uso: ./breach-parse.sh <dominio a buscar> <archivo de salida> [ubicaciÛn de datos de brechas]"
+    echo -e "${info} Ejemplo: ./breach-parse.sh @gmail.com gmail.txt"
+    echo -e "${info} Ejemplo: ./breach-parse.sh @gmail.com gmail.txt \"~/Downloads/BreachCompilation/data\""
     exit 1
 else
     if [ $# -ge 4 ]; then
-        echo 'You supplied more than 3 arguments, make sure to double quote your strings:'
-        echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/Temp Files/BreachCompilation"'
+        echo -e "${error} Has suministrado m·s de 3 argumentos. Aseg˙rate de poner entre comillas tus cadenas:"
+        echo -e "${info} Ejemplo: ./breach-parse.sh @gmail.com gmail.txt \"~/Downloads/Temp Files/BreachCompilation\""
         exit 1
     fi
 
-    # assume default location
+    # Asumir ubicaciÛn predeterminada
     breachDataLocation="/opt/breach-parse/BreachCompilation/data"
-    # check if BreachCompilation was specified to be somewhere else
     if [ $# -eq 3 ]; then
         if [ -d "$3" ]; then
             breachDataLocation="$3"
         else
-            echo "Could not find a directory at ${3}"
-            echo 'Pass the BreachCompilation/data directory as the third argument'
-            echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation/data"'
+            echo -e "\n${error} No se pudo encontrar un directorio en ${3}"
             exit 1
         fi
     else
         if [ ! -d "${breachDataLocation}" ]; then
-            echo "Could not find a directory at ${breachDataLocation}"
-            echo 'Put the breached password list there or specify the location of the BreachCompilation/data as the third argument'
-            echo 'Example: ./breach-parse.sh @gmail.com gmail.txt "~/Downloads/BreachCompilation/data"'
+            echo -e "\n${error} No se pudo encontrar un directorio en ${breachDataLocation}"
             exit 1
         fi
     fi
 
-    # set output filenames
+    # Establecer nombres de archivos de salida
     fullfile=$2
     fbname=$(basename "$fullfile" | cut -d. -f1)
     master=$fbname-master.txt
     users=$fbname-users.txt
     passwords=$fbname-passwords.txt
 
-    touch $master
-    # count files for progressBar
-    # -not -path '*/\.*' ignores hidden files/directories that may have been created by the OS
+    # Limpiar cualquier archivo existente para evitar acumulaciÛn de datos anteriores
+    > $master
+    > $users
+    > $passwords
+
     total_Files=$(find "$breachDataLocation" -type f -not -path '*/\.*' | wc -l)
     file_Count=0
 
     function ProgressBar() {
-
         let _progress=$(((file_Count * 100 / total_Files * 100) / 100))
         let _done=$(((_progress * 4) / 10))
         let _left=$((40 - _done))
@@ -61,28 +102,24 @@ else
         _fill=$(printf "%${_done}s")
         _empty=$(printf "%${_left}s")
 
-        printf "\rProgress : [${_fill// /\#}${_empty// /-}] ${_progress}%%"
-
+        printf "\n\r${indicator} Progreso : [${_fill// /\#}${_empty// /-}] ${_progress}%%"
     }
 
-    # grep for passwords
+    echo -e "\n${process} Buscando contraseÒas..."
     find "$breachDataLocation" -type f -not -path '*/\.*' -print0 | while read -d $'\0' file; do
-        grep -a -E "$1" "$file" >>$master
+        # Usar grep y eliminar duplicados en tiempo real antes de guardar en el archivo maestro
+        grep -a -E "$1" "$file" | sort -u >> $master
         ((++file_Count))
-        ProgressBar ${number} ${total_Files}
-
+        ProgressBar
     done
+
+    sleep 1
+    echo -e "\n\n${info} Extrayendo nombres de usuario..."
+    awk -F':' '{print $1}' $master | sort -u > $users  # Eliminar duplicados
+
+    sleep 1
+    echo -e "${info} Extrayendo contraseÒas..."
+    awk -F':' '{print $2}' $master | sort -u > $passwords  # Eliminar duplicados
+    echo -e "${checkmark} ExtracciÛn completada.${reset}\n"
+    exit 0
 fi
-
-sleep 3
-
-echo # newline
-echo "Extracting usernames..."
-awk -F':' '{print $1}' $master >$users
-
-sleep 1
-
-echo "Extracting passwords..."
-awk -F':' '{print $2}' $master >$passwords
-echo
-exit 0
