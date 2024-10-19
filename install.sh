47fbfcacbacf072ec1a78e6f039fbe860f6d4a7c
AAAAAEXQOSyIpN2JZ0ehUQ <36234074+AAAAAEXQOSyIpN2JZ0ehUQ@users.noreply.github.com>
2024-10-19 13:39:32 -0300

    install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,37 @@
 #!/bin/bash
 
-uid=$(id -u)
+#====================================================
+#   SCRIPT:                   Install Breach Parse
+#   DESARROLLADO POR:         Heath Adams (hmaverickadams)
+#   ACTUALIZADO POR:          Jony Rivera (Dzhoni)
+#   FECHA DE ACTUALIZACIÓN:   19-10-2024 
+#   CONTACTO POR TELEGRAMA:   https://t.me/Dzhoni_dev
+#====================================================
 
-if [ "$uid" -ne 0 ]
-    then echo "Error: To install, please run as root (uid 0)."
-    exit
+# Paleta de colores
+reset="\033[0m"       # Restablecer todos los estilos y colores
+green="\033[0;32m"    # Verde
+red="\033[0;31m"      # Rojo
+
+# Verificar si el script se está ejecutando como root
+if [ "$(id -u)" -ne 0 ]; then
+    echo -e "${red}Error: Para instalar, por favor ejecuta como root (uid 0).${reset}"
+    exit 1
 fi
 
-cp ./breach-parse.sh ./breach-parse
-mv ./breach-parse /usr/local/bin/
+# Verificar si el archivo de origen existe
+if [ ! -f "./breach-parse.sh" ]; then
+    echo -e "${red}Error: El archivo 'breach-parse.sh' no se encuentra en el directorio actual.${reset}"
+    exit 1
+fi
 
-echo "Installation complete. Usage: $ breach-parse"
+# Copiar el script a /usr/local/bin/
+cp ./breach-parse.sh /usr/local/bin/breach-parse
+
+# Verificar si la copia fue exitosa
+if [ $? -eq 0 ]; then
+    echo -e "${green}Instalación completada con éxito. Uso: breach-parse${reset}"
+else
+    echo -e "${red}Error: Falló la instalación.${reset}"
+    exit 1
+fi
